6861d3d356b84ddb523aaf499c86de9568cbbdd8
kingkey2 <132646006+kingkey2@users.noreply.github.com>
2023-05-05 17:45:11 +0800

    Add files via upload

---
+++ OcwSevenDayBonus/SettingObject/SettingObjectClass.cs
@@ -0,0 +1,8 @@
+﻿public class SettingObjectClass {
+    public bool IsTest = true;
+    public string ServerAPI = "http://ocw_ph.dev.mts.idv.tw/API/MgmtAPI.asmx";
+    public string ApiKey_Test = "0125d88e-7078-4be7-b1de-edd5bfcd41cc";
+    public string ApiKey = "b4a2dff1-da56-4763-a5d4-237b8ec832af";
+    //public bool IsTest = false;
+    //public string ServerAPI = "https://www.luckyfanta.com/API/MgmtAPI.asmx";
+}
